a52caa9eca0155fff67dd1f0e4a078a6e8e9475a
snerva <selenenerva@live.it>
2022-10-10 16:50:47 +0200

    order correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 //scrivi un programma che stampi in console numeri da 1 a 100
-for ( let i=1; 1<=100; i++) {
-  //per i multipli di 3 stampa Fizz
+for ( let i=1; i<=100; i++) {
+  //per i multipli sia di 3 che di 5 stampa FizzBuzz
   //if
-  if ((i%3) == 0){
-    console.log("Fizz");
-  //per i multipli di 5 stampa Buzz
+  if ((i%15) == 0){
+    console.log("FizzBuzz");
+  //per i multipli di 3 stampa Fizz
   //else if
+  } else if ((i%3) == 0){
+    console.log("Fizz");
+    //per i multipli di 5 stampa Buzz
+  //else if &&
   } else if ((i%5) == 0){
     console.log("Buzz");
-  //per i multipli sia di 3 che di 5 stampa FizzBuzz
-  //else if &&
-  } else if ((i%15) == 0){
-    console.log("FizzBuzz");
   //se non é multiplo di nessuno dei due allora stampa il numero
   //else
   } else {
