b6720f9e239a6b34b4862135cc39d874217e1f48
snerva <selenenerva@live.it>
2022-10-10 15:05:08 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+//scrivi un programma che stampi in console numeri da 1 a 100
+//per i multipli di 3 stampa Fizz
+//per i multipli di 5 stampa Buzz
+//per i multipli sia di 3 che di 5 stampa FizzBuzz
